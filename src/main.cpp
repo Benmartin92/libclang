f7fe54885e1395d7459739231256ffa63ae348f1
Benjamin Seregi <seregibenjamin@gmail.com>
2019-03-08 18:10:18 -0500

    [src]: added a test file to parse.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+enum Fruits {
+    APPLE = 1,
+    ORANGE,
+    BANANA,
+    LEMON
+};
+
+enum MathematicalFruits {
+    TWO_APPLES = 2*APPLE,
+    LEMON_PLUS_ORANGE  = LEMON + ORANGE
+};
+
+int main() {
+    return 0;
+}
